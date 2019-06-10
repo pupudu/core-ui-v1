841102a53d60457ae843fea9d4ab1e1394041ecd
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-06-10 18:15:50 +1000

    Migrate to react-scripts

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -28,7 +28,7 @@ const getPaths = (pathname) => {
   return paths;
 };
 
-const BreadcrumbsItem = ({...rest, match}) => {
+const BreadcrumbsItem = ({match, ...rest}) => {
   const routeName = findRouteName(match.url);
   if (routeName) {
     return (
@@ -48,7 +48,7 @@ const BreadcrumbsItem = ({...rest, match}) => {
   return null;
 };
 
-const Breadcrumbs = ({...rest, location : {pathname}, match}) => {
+const Breadcrumbs = ({location : {pathname}, match, ...rest}) => {
   const paths = getPaths(pathname);
   const items = paths.map((path, i) => <Route key={i++} path={path} component={BreadcrumbsItem}/>);
   return (
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import './polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 
 // Styles
-// Import Flag Icons Set
-import 'flag-icon-css/css/flag-icon.min.css';
 // Import Font Awesome Icons Set
 import 'font-awesome/css/font-awesome.min.css';
-// Import Simple Line Icons Set
-import 'simple-line-icons/css/simple-line-icons.css';
 // Import Main styles for this application
-import '../scss/style.scss'
+import './scss/style.scss'
 // Temp fix for reactstrap
-import '../scss/core/_dropdown-menu-right.scss'
+import './scss/core/_dropdown-menu-right.scss'
 
 // Containers
 import Full from './containers/Full/'
