f32cd51343b39e084474c4f8f0c8a506f53e7a6d
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-06-13 17:12:46 +1000

    More cleanup

--- src/App/Dashboard.js
+++ src/App/Dashboard.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function Dashboard() {
-  return <div className="animated fadeIn">Hello World</div>;
+  return (
+    <div className="animated fadeIn">
+      <button className="btn btn-primary">Primary</button>{' '}
+      <label className="switch switch-default switch-success">
+        <input type="checkbox" className="switch-input" defaultChecked />
+        <span className="switch-label" />
+        <span className="switch-handle" />
+      </label>
+    </div>
+  );
 }
 
 export default Dashboard;
