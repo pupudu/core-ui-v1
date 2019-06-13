d2de0f3ea1af7000c0cc96c1106b210cae5fb518
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-06-13 19:11:20 +1000

    More and more cleanup

--- src/App/Dashboard.js
+++ src/App/Dashboard.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const ModalButton = () => {
+  const [isOpen, setOpen] = useState(false);
+  const toggle = () => setOpen(!isOpen);
+  return (
+    <>
+      <Button color="danger" onClick={toggle}>
+        Open
+      </Button>
+      <Modal isOpen={isOpen} toggle={toggle} className="modal-danger">
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>Lorem ipsum</ModalBody>
+        <ModalFooter>
+          <Button color="info" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+};
 
 function Dashboard() {
   return (
     <div className="animated fadeIn">
-      <button className="btn btn-primary">Primary</button>{' '}
-      <label className="switch switch-default switch-success">
-        <input type="checkbox" className="switch-input" defaultChecked />
-        <span className="switch-label" />
-        <span className="switch-handle" />
-      </label>
+      <div className="card">
+        <div className="card-header">Heading</div>
+        <div className="card-body">
+          <button className="btn btn-secondary">Primary</button>{' '}
+          <button className="btn btn-teal">Primary</button> <ModalButton />{' '}
+          <label className="switch switch-default switch-success">
+            <input type="checkbox" className="switch-input" defaultChecked />
+            <span className="switch-label" />
+            <span className="switch-handle" />
+          </label>
+        </div>
+        <div className="card-footer">Footer</div>
+      </div>
     </div>
   );
 }
