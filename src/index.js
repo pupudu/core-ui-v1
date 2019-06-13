b3238a3c53d5547b159d7690c7fc69893cd6b56b
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-06-13 16:52:07 +1000

    Cleanup scss

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Styles
-// Import Font Awesome Icons Set
-import 'font-awesome/css/font-awesome.min.css';
-// Import Main styles for this application
 import './scss/style.scss';
-// Temp fix for reactstrap
-import './scss/core/_dropdown-menu-right.scss';
 
 // Containers
 import App from './App';
