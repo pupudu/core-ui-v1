16ef34c178963cdbb8eef09e1bc01fc882d88a6f
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-06-13 11:25:25 +1000

    Convert class component to function component

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,46 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavbarToggler, NavbarBrand } from 'reactstrap';
 
-class Header extends Component {
-  sidebarToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('sidebar-hidden');
-  }
+function sidebarToggle(e) {
+  e.preventDefault();
+  document.body.classList.toggle('sidebar-hidden');
+}
 
-  sidebarMinimize(e) {
-    e.preventDefault();
-    document.body.classList.toggle('sidebar-minimized');
-  }
+function mobileSidebarToggle(e) {
+  e.preventDefault();
+  document.body.classList.toggle('sidebar-mobile-show');
+}
 
-  mobileSidebarToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('sidebar-mobile-show');
-  }
+function asideToggle(e) {
+  e.preventDefault();
+  document.body.classList.toggle('aside-menu-hidden');
+}
 
-  asideToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('aside-menu-hidden');
-  }
-
-  render() {
-    return (
-      <header className="app-header navbar">
-        <NavbarToggler className="d-lg-none" onClick={this.mobileSidebarToggle}>
-          <span className="navbar-toggler-icon" />
-        </NavbarToggler>
-        <NavbarBrand href="#" />
-        <NavbarToggler
-          className="d-md-down-none mr-auto"
-          onClick={this.sidebarToggle}
-        >
-          <span className="navbar-toggler-icon" />
-        </NavbarToggler>
-        <NavbarToggler className="d-md-down-none" onClick={this.asideToggle}>
-          <span className="navbar-toggler-icon" />
-        </NavbarToggler>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className="app-header navbar">
+      <NavbarToggler className="d-lg-none" onClick={mobileSidebarToggle}>
+        <span className="navbar-toggler-icon" />
+      </NavbarToggler>
+      <NavbarBrand href="#" />
+      <NavbarToggler className="d-md-down-none mr-auto" onClick={sidebarToggle}>
+        <span className="navbar-toggler-icon" />
+      </NavbarToggler>
+      <NavbarToggler className="d-md-down-none" onClick={asideToggle}>
+        <span className="navbar-toggler-icon" />
+      </NavbarToggler>
+    </header>
+  );
 }
 
 export default Header;
